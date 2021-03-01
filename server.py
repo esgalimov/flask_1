4cf001896ff045ee2833a64173596dee8fc0793f
esgalimov <esgalimov@gmail.com>
2021-03-01 23:10:22 +0300

    WEB. Введение во flask. Задача 6. Варианты выбора. server.py

--- server.py
+++ server.py
@@ -188,5 +188,39 @@ def form():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet"
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                       crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}</h1>
+                        <div class="alert alert-dark" role="alert">
+                            <h3>Эта планета близка к Земле;</h3>
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                             <h3>На ней много необходимых ресурсов;</h3>
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            <h3>На не есть вода и атмосфера;</h3>
+                        </div>
+                        <div class="alert alert-info" role="alert">
+                             <h3>На ней есть небольшое магнитное поле;</h3>
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                             <h3>Наконец, она просто красива!</h3>
+                        </div>
+                      </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

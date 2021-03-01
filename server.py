87a18b01bd5d5cfbebf09917a75e54b1c9265d78
esgalimov <esgalimov@gmail.com>
2021-03-01 23:20:36 +0300

    WEB. Введение во flask. Задача 7. Результат отбора. server.py

--- server.py
+++ server.py
@@ -222,5 +222,35 @@ def choice(planet_name):
                     </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                           <link rel="stylesheet"
+                           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                           crossorigin="anonymous">
+                            <title>Результаты</title>
+                          </head>
+                          <body>
+                            <h1>Результаты отбора</h1>
+                            <h2>Претендент на участие в миссии {nickname}:</h2>
+                            <div class="alert alert-success" role="alert">
+                                 <h3>Поздравляем! Ваш рейтинг после {level} этапа отбора</h3>
+                            </div>
+                            <div class="alert alert-light" role="alert">
+                                <h3>составляет {rating}!</h3>
+                            </div>
+                            <div class="alert alert-warning" role="alert">
+                                 <h3>Желаем удачи!</h3>
+                            </div>
+                            </div>
+                          </body>
+                        </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
